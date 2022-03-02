1e5bffa886f877246aae2ab59a04b614e7af9c13
Rabbi Ahmed Limon <rabbiahemed>
2022-03-02 23:16:18 +0600

    optional chaining

--- phone-finder.js
+++ phone-finder.js
@@ -15,10 +15,12 @@ const displaySearchResult = phones => {
         const searchResult = document.getElementById("search-result");
         const div = document.createElement('div');
         div.innerHTML = ` <div class="card h-100 text-center shadow-lg">
+        <div class="card-body">
         <img id="phone-image" src="${phone.image}" class="card-img-top img-fluid w-25 h-50" alt="...">
         <h3>Name:${phone.phone_name}</h3>
         <p>Brand: ${phone.brand}</p>
         <button id="details-btn" onclick="loadDetails('${phone.slug}')" class="w-50 mx-auto mb-3 p-2 rounded"> See Details</button >
+        </div>
     </div > `
         searchResult.appendChild(div);
     }
@@ -31,12 +33,30 @@ const loadDetails = (phoneId) => {
         .then(data => showDetails(data.data))
 }
 const showDetails = (detail) => {
-    const div = document.getElementById("phone-details");
-    div.innerHTML = `<div class="card h-100 text-center shadow-lg mx-auto">
-        <img id="phone-image" src="${detail.image}" class="card-img-top img-fluid w-25 h-25" alt="...">
-            <p class="text-center pt-3 fw-bold">llsdksl</p>
-            <h5>Name:${detail.name}</h5>
-            <h5>Realease Date :${detail.releaseDate}</h5>
-    </div >`
-    console.log(detail);
+    const detailDiv = document.getElementById("phone-details");
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="text-center shadow-lg mx-auto mt-2">
+    <div class="card h-100">
+        <img id="phone-image" src="${detail.image}" class="card-img-top img-fluid w-25 h-25 mb-4" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Name:${detail.name}</h5>
+            <h5 class="card-text">Brand:${detail.brand}</h5>
+            <h5 class="card-text">Released Date:${detail.releaseDate ? detail.releaseDate : 'no release date found'}</h5>
+            <p class="text-center pt-3 fw-bold card-text"><u>Main Feauters</u></p>
+            <p class="card-text">Storage:${detail.mainFeatures.storage}</p>
+            <h class="card-text">Display:${detail.mainFeatures.displaySize}</p>
+            <h class="card-text">Chipset:${detail.mainFeatures.chipSet}</p>
+            <h class="card-text">Memory:${detail.mainFeatures.memory}</p>
+            <h class="card-text">Sensor:${detail.mainFeatures.sensors.join(', ')}</p>
+            <p class="card-text">Others:</p>
+            <h class="card-text">WLAN:${detail.others?.WLAN ? detail.others.WLAN : 'No release date found'}</p>
+            <h class="card-text">Bluetooth:${detail.others?.bluetooth ? detail.others.bluetooth : 'No information found'}</p>
+            <h class="card-text">GPS:${detail?.others?.GPS ? detail.others.GPS : 'No information found'}</p>
+            <h class="card-text">NFC:${detail?.others?.NFC ? detail.others.NFC : 'No information foind'}</p>
+            <h id="radio" class="card-text">Radio:${detail?.others?.Radio ? detail.others.Radio : 'No information found'}</p>
+            <h class="card-text">USB:${detail?.others?.USB ? detail.others.USB : 'No information found'}</p>    
+        </div>
+    </div > `
+    detailDiv.appendChild(div);
+    console.log(detail, detail[0]);
 }
